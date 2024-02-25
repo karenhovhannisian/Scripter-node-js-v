ac80166049b724b97e8ef67c7ea0054da76bfe25
gaborm <126419721+GaboRM9@users.noreply.github.com>
2024-02-25 10:50:07 -0600

    Auto deploy

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ app.post('/CreateScripter', async (req, res) => {
     const vectorStore = await MemoryVectorStore.fromDocuments(splits, new OpenAIEmbeddings());
     const retriever = vectorStore.asRetriever();
 
-    //Create query for models
+    //Create query for models 
     const llm = new ChatOpenAI({ modelName: "gpt-3.5-turbo-0125", temperature: 0 });   //Replace model for the fine-tuned version
     const prompt = ChatPromptTemplate.fromTemplate(`
         Answer the following question based only on the provided context:
