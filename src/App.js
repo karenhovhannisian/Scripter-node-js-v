2f7e177551a29de8f310a7fb191d7f979caaa6ff
gaborm <126419721+GaboRM9@users.noreply.github.com>
2024-02-25 10:32:10 -0600

    Comment clean up

--- src/App.js
+++ src/App.js
@@ -20,32 +20,6 @@ const port = process.env.PORT || 3000; // load port from .env or default to 3000
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
 
-/* API auth handler - Enable on production.
-// Middleware to check for API key in request headers
-const apiKeyMiddleware = (req, res, next) => {
-    const apiKey = req.headers['x-api-key']; // Assuming the API key is passed in the 'x-api-key' header
-
-    if (!apiKey) {
-        return res.status(401).json({ error: 'Please provide a valid API Key' });
-    }
-
-    if (apiKey !== process.env.API_KEY) { // Assuming your valid API key is stored in an environment variable
-        return res.status(403).json({ error: 'Invalid API key' });
-    }
-
-    next(); // Proceed to the next middleware/function if the API key is valid
-};
-
-        Answer the following question based only on the provided context:
-        <context>{context}</context>
-        You should answer the code question giving an example to the user, use concise responses dont invent stuff.
-        The format of your response is:
-        Consice short explanation, no line breaks.
-        <code>Code example</code>
-        If the question is not related to QuestionProJs logics, kindly remind the user that you can help only on QuestionPro Javascript logics questions at the moment & invite the user to our help center: https://www.questionpro.com/help/ (dont use the same text always to this, create a short invitation) "
-        Question: {input}
-*/
-
 // POST route to accept input
 app.post('/CreateScripter', async (req, res) => {
     const inputText = req.body.input; // Access the input from the request body
